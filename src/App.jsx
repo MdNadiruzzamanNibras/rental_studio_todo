d48f7b2e92bfb33054effc000ada5b7b254998f0
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-10-12 12:11:57 +0600

    from

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 
 
+
 const App = () => {
   return (
-    <div>
-      
-    </div>
+     <div className='card w-96 bg-base-100 shadow-xl my-20 mx-auto' >
+        <div className="card-body">
+           <h1 className='text-center text-3xl font-bold'>Task List</h1>
+            <form className='flex justify-center items-center'>
+                <input type="text" name='todo' className="input input-bordered "  
+          
+         />
+                <button className='btn btn-active my-3 flex justify-center'>Add Task</button>
+            </form>
+            
+        </div>
+       
+        </div>
   );
 };
 
