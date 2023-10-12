a42de58e801ebcc8ba173860a1d54ff91ba8905c
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-10-12 12:46:08 +0600

    data post

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
 const App = () => {
+  const additem = async(e) =>{
+        e.preventDefault();
+        const todo = e?.target?.todo?.value
+        const todolist={
+           
+            todo: todo
+        }
+        fetch('http://localhost:5000/todos',{
+            method:'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body:JSON.stringify(todolist)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            if(data.insertedId){
+                toast.success('Great add your task item')
+            }
+            else{
+                toast.error('Something is Wrong')
+                
+            }
+
+        })
+    }
   return (
      <div className='card w-96 bg-base-100 shadow-xl my-20 mx-auto' >
         <div className="card-body">
@@ -10,11 +39,11 @@ const App = () => {
                 <input type="text" name='todo' className="input input-bordered "  
           
          />
-                <button className='btn btn-active my-3 flex justify-center'>Add Task</button>
+                <button className='btn btn-active ml-3 flex justify-center' onClick={additem}>Add Task</button>
             </form>
             
         </div>
-       
+       <ToastContainer/>
         </div>
   );
 };
